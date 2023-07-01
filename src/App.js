d77326aa1d7f49cdd735cbfaa6fae4667eb1a3d1
Reid <touyalin@gmail.com>
2023-07-01 23:59:31 +0800

    6.63 adding another piece of state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const App = () => {
   const messages = [
@@ -7,6 +7,8 @@ const App = () => {
     "Invest your new income 🤑",
   ];
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   const handlePrevious = () => {
     if (step > 1) {
       setStep(step - 1);
@@ -17,34 +19,44 @@ const App = () => {
       setStep(step + 1);
     }
   };
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <React.Fragment>
+      <button className="close" onClick={handleIsOpen}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
